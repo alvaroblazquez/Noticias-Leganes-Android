b1f4826c48670e7e2db55a6ab46d396cd5471c55
Alvaro Blazquez Montero <alvarob25@gmail.com>
2017-10-29 14:37:41 +0100

    Changes in DateFormat

--- app/src/main/java/nural/smart/cdleganes/match/Match.kt
+++ app/src/main/java/nural/smart/cdleganes/match/Match.kt
@@ -1,23 +1,24 @@
 package nural.smart.cdleganes.match
 
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 /**
- * Created by alvaro on 13/10/17.
- */
+* Created by alvaro on 13/10/17.
+*/
 
 enum class Status {
     OPENED, FINISHED, TIMED, SCHEDULED
 }
 
-enum class Winner {
+/*enum class Winner {
     HOME, AWAY, DRAW
-}
+}*/
 
 
 data class Match(
-        val date          : String?,
+        private val date          : String?,
         val status        : Status = Status.OPENED,
         val homeTeamName  : String = "",
         val awayTeamName  : String = "",
@@ -25,23 +26,25 @@ data class Match(
     ) {
     var dateFormatted: String = ""
         get() {
-            var dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+
             dateFormat.timeZone = TimeZone.getTimeZone("Etc/GMT")
             val dateMatch = dateFormat.parse(this.date)
 
-            dateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm")
-            return dateFormat.format(dateMatch)
+            return DateFormat.
+                    getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.FRANCE).
+                    format(dateMatch)
         }
 }
 
 data class MatchList(val fixtures: List<Match>)
 
 data class Result(val goalsHomeTeam : Int, val goalsAwayTeam: Int) {
-    var winner: Winner = Winner.AWAY
+    /*var winner: Winner = Winner.AWAY
         get() {
             if (this.goalsHomeTeam == this.goalsAwayTeam) {
                 return Winner.DRAW
             }
             return if (this.goalsHomeTeam > this.goalsAwayTeam) Winner.HOME else Winner.AWAY
-        }
+        }*/
 }
